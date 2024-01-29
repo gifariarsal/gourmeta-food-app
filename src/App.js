a8362a52d3b77753c04efd4de7217c2b86117e75
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-01-29 17:53:37 +0800

    update: navbar, hero, icons, data, and color style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Category, Food, Headline, Hero, Navbar } from "./components";
 
 function App() {
   return (
     <>
+    <Navbar />
+    <Hero />
+    <Headline />
+    <Food />
+    <Category />
     </>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Category from './Category';
+import Food from './Food';
+import Headline from './Headline';
+import Hero from './Hero';
+import Navbar from './Navbar';
+
+export { Category, Food, Headline, Hero, Navbar };
