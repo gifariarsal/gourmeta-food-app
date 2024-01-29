a093aa04d382b7259c48e12ae9a7f8f4bff9e021
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-01-29 22:13:14 +0800

    add: logo, edit: content and styling in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import {
-  AiOutlineMenu,
-  AiOutlineSearch,
-  AiOutlineClose,
-  AiFillTag,
-} from 'react-icons/ai';
-import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs';
-import { TbTruckDelivery } from 'react-icons/tb';
-import { FaUserFriends, FaWallet } from 'react-icons/fa';
-import { MdFavorite, MdHelp } from 'react-icons/md';
+  IoMenuOutline,
+  IoClose,
+  IoSearch,
+  IoCartOutline,
+  IoReceiptOutline,
+  IoHeartOutline,
+  IoWalletOutline,
+  IoPricetagsOutline,
+  IoHelpCircleOutline,
+} from 'react-icons/io5';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -16,24 +17,25 @@ const Navbar = () => {
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div onClick={() => setNav(!nav)} className="cursor-pointer">
-          <AiOutlineMenu size={30} />
+        <div
+          onClick={() => setNav(!nav)}
+          className="cursor-pointer p-2 rounded-full hover:bg-gray-200 transition duration-300 ease-in-out"
+        >
+          <IoMenuOutline size={30} />
         </div>
-        <h1 className="text-primary font-bold text-2xl sm:text-3xl lg:text-4xl px-2">
-          Logo
-        </h1>
+        <img src="/gourmeta_logo.png" alt="logo" className="ml-2 h6 md:h-8" />
       </div>
 
       <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
-        <AiOutlineSearch size={25} />
+        <IoSearch size={25} />
         <input
           className="bg-transparent p-2 w-full focus:outline-none"
           type="text"
-          placeholder="Search foods"
+          placeholder="Search foods ..."
         />
       </div>
-      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
-        <BsFillCartFill size={20} className="mr-2" /> Cart
+      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full hover:bg-gray-700 transition duration-300 ease-in-out">
+        <IoCartOutline size={20} className="mr-2" /> Cart
       </button>
 
       {nav ? (
@@ -45,40 +47,32 @@ const Navbar = () => {
       <div
         className={
           nav
-            ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'
-            : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'
+            ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-500'
+            : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-500'
         }
       >
-        <AiOutlineClose
+        <IoClose
           onClick={() => setNav(!nav)}
           size={30}
           className="absolute right-4 top-4 cursor-pointer"
         />
-        <h2 className="text-2xl p-4">
-          Best <span className="font-bold">Eats</span>
-        </h2>
+        <h2 className="text-primary text-3xl p-4">gourmeta</h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-800">
             <li className="text-xl py-4 flex">
-              <TbTruckDelivery size={25} className="mr-4" /> Orders
+              <IoReceiptOutline size={25} className="mr-4" /> Orders
             </li>
             <li className="text-xl py-4 flex">
-              <MdFavorite size={25} className="mr-4" /> Favorites
+              <IoHeartOutline size={25} className="mr-4" /> Favorites
             </li>
             <li className="text-xl py-4 flex">
-              <FaWallet size={25} className="mr-4" /> Wallet
+              <IoWalletOutline size={25} className="mr-4" /> Wallet
             </li>
             <li className="text-xl py-4 flex">
-              <MdHelp size={25} className="mr-4" /> Help
+              <IoPricetagsOutline size={25} className="mr-4" /> Promotions
             </li>
             <li className="text-xl py-4 flex">
-              <AiFillTag size={25} className="mr-4" /> Promotions
-            </li>
-            <li className="text-xl py-4 flex">
-              <BsFillSaveFill size={25} className="mr-4" /> Best Ones
-            </li>
-            <li className="text-xl py-4 flex">
-              <FaUserFriends size={25} className="mr-4" /> Invite Friends
+              <IoHelpCircleOutline size={25} className="mr-4" /> Help
             </li>
           </ul>
         </nav>
