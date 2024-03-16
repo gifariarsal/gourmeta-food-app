52c762a061e028e1aac35cb94fac208cd7683096
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-03-16 23:04:49 +0800

    refactor(Category): destructure categories, use id for keys
    - Applied destructuring to the categories properties to shorten code syntax and improve readability.
    - Replaced the use of index as keys with unique id values to enhance list rendering performance and consistency.

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -9,13 +9,13 @@ const Category = () => {
         Menu Categories
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
-        {categories.map((item, index) => (
+        {categories.map(({ id, name, image }) => (
           <div
-            key={index}
+            key={id}
             className="bg-gray-100 rounded-lg border shadow-md cursor-pointer p-4 flex justify-between items-center hover:scale-105 duration-300"
           >
-            <h2 className="font-bold sm:text-xl">{item.name}</h2>
-            <img src={item.image} alt={item.name} className="w-20" />
+            <h2 className="font-bold sm:text-xl">{name}</h2>
+            <img src={image} alt={name} className="w-20" />
           </div>
         ))}
       </div>
